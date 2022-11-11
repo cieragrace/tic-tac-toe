b92b7e6110dbd632ad9137c42486c9540a9ed8d8
Ciera DePauw <113853138+cieragrace@users.noreply.github.com>
2022-11-11 08:23:03 -0700

    Add pseudocoding for main.js

--- main.js
+++ main.js
@@ -1,19 +1,57 @@
 // -------> query selectors <-------
-var playerContainer = document.querySelector('.player-container')
-var players = document.querySelector('.player')
-var playersScores = document.querySelector('.player-score')
-var headContainer = document.querySelector('.head-container')
-var playerBanner = document.querySelector('.player-banner')
-var announceBanner = document.querySelector('.its-your-turn')
-var gridContainer = document.querySelector('.total-grid')
-var Box1 = document.querySelector('.box-1')
-var Box2 = document.querySelector('.box-2')
-var Box3 = document.querySelector('.box-3')
-var Box4 = document.querySelector('.box-4')
-var Box5 = document.querySelector('.box-5')
-var Box6 = document.querySelector('.box-6')
-var Box7 = document.querySelector('.box-7')
-var Box8 = document.querySelector('.box-8')
-var Box9 = document.querySelector('.box-9')
+// var playerContainer = document.querySelector('.player-container')
+// var players = document.querySelector('.player')
+// var playersScores = document.querySelector('.player-score')
+// var headContainer = document.querySelector('.head-container')
+// var playerBanner = document.querySelector('.player-banner')
+// var announceBanner = document.querySelector('.its-your-turn')
+// var gridContainer = document.querySelector('.total-grid')
+// var Box1 = document.querySelector('.box-1')
+// var Box2 = document.querySelector('.box-2')
+// var Box3 = document.querySelector('.box-3')
+// var Box4 = document.querySelector('.box-4')
+// var Box5 = document.querySelector('.box-5')
+// var Box6 = document.querySelector('.box-6')
+// var Box7 = document.querySelector('.box-7')
+// var Box8 = document.querySelector('.box-8')
+// var Box9 = document.querySelector('.box-9')
 
 // -------> event listeners <-------
+
+// -------> functions <-------
+
+// need reset button
+// do I need a load page event listener?
+// do the 9 tiles need to be 9 different classes? 
+
+// start with X, first tile is X
+// 0 is second
+// event listeners on all tiles
+// input on tiles?
+
+// if winning three positions are marked 'true' x or o wins
+// when there is a winner we want the score to go up 1 on winner side
+// we also will need banner to pop up to say who wins
+// change "It's your turn" to WINS
+// have pop up that announces winner 
+//Player 1, YOU WIN
+//PLAYER 2, YOU WIN
+//DRAW
+//DRAW scores do not change
+//time out to reset page 
+
+// if box && box && box are true = winner 
+
+//board layout below
+    // [1][2][3]
+    // [4][5][6]
+    // [7][8][9]
+// winning combos below
+    // [1][2][3]
+    // [4][5][6]
+    // [7][8][9]
+    // [1][4][7]
+    // [2][5][8]
+    // [3][6][9]
+    // [1][5][9]
+    // [3][5][7]
