d5fe91250250994dc6c76e8f55506cd57ac62ab4
Ciera DePauw <113853138+cieragrace@users.noreply.github.com>
2022-11-13 11:30:43 -0700

    Update game class vs rubric

--- game.js
+++ game.js
@@ -4,8 +4,30 @@ class Game {
     var player2 = new Player(this.id: player2, this.symbol: 'O', total.wins: 0) 
    }
    // a way to keep track of data of game board
+   var gameBoard;
    //a way to track who's turn it is
+   function playerXTurn() {
+
+   }
+   function playerOTurn() {
+
+   }
    //a way to check game board data for win conditions
+   // winning combos below
+    // var winningCombos = 
+    // [1][2][3],
+    // [4][5][6],
+    // [7][8][9],
+    // [1][4][7],
+    // [2][5][8],
+    // [3][6][9],
+    // [1][5][9],
+    // [3][5][7],
+
+    function checkForWinner() {
+
+    }
    //a way to detect when a game is a draw
    //a way to reset the board for new game
-}
+//    function startGame() {
+
