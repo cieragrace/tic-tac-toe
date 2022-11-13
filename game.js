d1cafaec2ee440eb1902380795f53b801465e6f3
Ciera DePauw <113853138+cieragrace@users.noreply.github.com>
2022-11-13 10:29:48 -0700

    Add player instances and pseudocode

---
+++ game.js
@@ -0,0 +1,11 @@
+class Game {
+    constructor(){
+    var player1 = new Player(this.id: player1, this.symbol: 'X', this.wins: 0)
+    var player2 = new Player(this.id: player2, this.symbol: 'O', total.wins: 0) 
+   }
+   // a way to keep track of data of game board
+   //a way to track who's turn it is
+   //a way to check game board data for win conditions
+   //a way to detect when a game is a draw
+   //a way to reset the board for new game
+}
