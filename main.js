4b6bd92fcca1dd20ba43aac6d17bf920d6c6d619
Ciera DePauw <113853138+cieragrace@users.noreply.github.com>
2022-11-13 20:32:29 -0700

    Add to functions, still working on DOM

--- main.js
+++ main.js
@@ -5,31 +5,76 @@
 // var headContainer = document.querySelector('.head-container')
 // var playerBanner = document.querySelector('.player-banner')
 // var announceBanner = document.querySelector('.its-your-turn')
-// var gridContainer = document.querySelector('.total-grid')
-// var Box1 = document.querySelector('.box-1')
-// var Box2 = document.querySelector('.box-2')
-// var Box3 = document.querySelector('.box-3')
-// var Box4 = document.querySelector('.box-4')
-// var Box5 = document.querySelector('.box-5')
-// var Box6 = document.querySelector('.box-6')
-// var Box7 = document.querySelector('.box-7')
-// var Box8 = document.querySelector('.box-8')
-// var Box9 = document.querySelector('.box-9')
-// var resetButton = document.querySelector('.reset-button')
+var boxes = document.querySelectorAll('.box')
+var box1 = document.querySelector('#box1')
+var box2 = document.querySelector('#box2')
+var box3 = document.querySelector('#box3')
+var box4 = document.querySelector('#box4')
+var box5 = document.querySelector('#box5')
+var box6 = document.querySelector('#box6')
+var box7 = document.querySelector('#box7')
+var box8 = document.querySelector('#box8')
+var box9 = document.querySelector('#box9')
+var resetButton = document.querySelector('.reset-button')
 
 // -------> event listeners <-------
-window.addEventListener('load, startGame')
+window.addEventListener('load', startGame)
 resetButton.addEventListener('click', startGame)
-// ------> global variables <------
+boxes.addEventListener('click', executeTurn)
 
+// ------> global variables <------
+var whosTurn = 'X'
+var gameBoardSpaces = ["", "", "", "", "", "", "", "", "",]
+var gameOn = false
+var winningCombos = [
+[0][1][2],
+[3][4][5],
+[6][7][8],
+[0][3][6],
+[1][4][7],
+[2][5][8],
+[0][4][8],
+[2][4][6],
+]
+// or
+// [0, 1, 2],
+// [3, 4, 5],
+// [6, 7, 8],
+// [0, 3, 6],
+// [1, 4, 7],
+// [2, 5, 8],
+// [0, 4, 8],
+// [2, 4, 6],
 // -------> functions <-------
 
 function startGame() {
-gameBoardSpaces = ["", "", "", "", "", "", "", "", "",]
+gameBoardSpaces = ["", "", "", "", "", "", "", "", "",];
+    boxes.forEach(box => box.addEventListener('click', executeTurn))
 }
-// need reset button -- use startGame function
-// do I need a load page event listener? -- done
-// do the 9 tiles need to be 9 different classes? --keep for now
+// use with reset button as well
+
+function executeTurn(player) {
+    if (isBoxOpen === true && gameOn === true) {
+        boxes.innerText = player.symbol
+    }
+}
+function isBoxOpen() {
+    if (boxes.innerText === 'X' || boxes.innerText === 'O') {
+        return false;
+    }
+    return true;
+}
+
+// function changePlayer() { // render?
+    var currentPlayer = new Player(id, symbol, wins)
+     currentPlayer = 'X' ? 'O' : 'X';
+
+// }
+    //if the tile is clicked on and is not already marked 
+    // should change inner text of current player
+    // add symbol for player. Check for winner, if no winner
+    // change player if there is a winner/draw execute banner 
+    // timeout and reset
 
 // start with X, first tile is X
 // 0 is second
@@ -37,54 +82,47 @@ gameBoardSpaces = ["", "", "", "", "", "", "", "", "",]
 // input on tiles?
 
 // if winning three positions are marked 'true' x or o wins
-function checkForWinner() {
+// function checkForWinner() {
 
-}
+// }
 //ALSO IN GAME CLASS
 // when there is a winner we want the score to go up 1 on winner side
-increaseWins() {
-
-}
+// increaseWins(player) {
+//     player.wins += 1
+// }
 //IN PLAYER CLASS
 // we also will need banner to pop up to say who wins
 // change "It's your turn" to WINS
 // have pop up that announces winner 
-function winningBanner() {
+// function winningBanner() {
 
-}
+// timeOut()
+// }
 //Player 1, YOU WIN
 //PLAYER 2, YOU WIN
-function announceWinner() {
+// function announceWinner() {
 
-}
+// }
 //DRAW
 //DRAW scores do not change
-function announceDraw() {
+// function announceDraw() {
 
-}
+// }
 
-function drawBanner() {
+// function drawBanner() {
 
-}
+// timeOut()
+// }
 // No Winner! Try Again
-function timeOut() {
-
-}
+// function timeOut() {
+//     setTimeout(startGame, 3000)
+// }
 //time out to reset page 
 
 // if box && box && box are true = winner 
 
 
 //board layout below
-    // [1][2][3]
-    // [4][5][6]
-    // [7][8][9]
-// winning combos below
-    // [1][2][3]
-    // [4][5][6]
-    // [7][8][9]
-    // [1][4][7]
-    // [2][5][8]
-    // [3][6][9]
-    // [1][5][9]
-    // [3][5][7]
+    // [0][1][2]
+    // [3][4][5]
+    // [6][7][8]
