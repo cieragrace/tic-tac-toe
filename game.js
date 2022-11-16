1f663c562d4e009b21acccd29e219e9179f8e52d
Ciera DePauw <113853138+cieragrace@users.noreply.github.com>
2022-11-16 12:31:05 -0700

    Refactor reset board function

--- game.js
+++ game.js
@@ -1,58 +1,87 @@
 class Game {
-    constructor() {
-    this.player1 = new Player(1, "X")
-    this.player2 = new Player(2, "O")
-    this.isGameLive = true
-    this.turns = 0
-    this.currentTurn = 'X'
-    this.winnerFound = false
-    this.gameBoardSpaces = [0, 0, 0, 0, 0, 0, 0, 0, 0]
-    this.draw = false
-    }
+  constructor() {
+    this.player1 = new Player(1, "X");
+    this.player2 = new Player(2, "O");
+    this.isGameLive = true;
+    this.turns = 0;
+    this.firstPlayer = "X";
+    this.currentTurn = "X";
+    this.winnerFound = false;
+    this.gameBoardSpaces = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+    this.draw = false;
+  }
 
-    changeTurns() {
+  changeTurns() {
+    console.log("change turns");
     if (this.currentTurn === "X") {
-        this.currentTurn = "O"
+      this.currentTurn = "O";
+      console.log("change turns if");
     } else {
-        this.currentTurn = "X"
-        }
+      this.currentTurn = "X";
+      console.log("change turns else");
     }
+  }
 
-    checkForWinner(player) {
+  checkForWinner() {
     var winningCombos = [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        [0, 4, 8],
-        [2, 4, 6],
-        ]
-        for (var i = 0; i < winningCombos.length; i++) {
-        if (player.boardPosition.includes(winningCombos[i][0]) && player.boardPosition.includes(winningCombos[i][1]) && player.boardPosition.includes(winningCombos[i][2])) {
-            player.wins += 1
-            this.winnerFound = true
-            this.isGameLive = false
-            }
-        }
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (var i = 0; i < winningCombos.length; i++) {
+      if (
+        this.player1.boardPosition.includes(winningCombos[i][0]) &&
+        this.player1.boardPosition.includes(winningCombos[i][1]) &&
+        this.player1.boardPosition.includes(winningCombos[i][2])
+      ) {
+        this.winnerFound = true;
+        this.player1.wins += 1;
+        this.isGameLive = false;
+      } else if (
+        this.player2.boardPosition.includes(winningCombos[i][0]) &&
+        this.player2.boardPosition.includes(winningCombos[i][1]) &&
+        this.player2.boardPosition.includes(winningCombos[i][2])
+      ) {
+        this.winnerFound = true;
+        this.player2.wins += 1;
+        this.isGameLive = false;
+        console.log(this.player1.wins);
+        console.log(this.player2.wins);
+      }
     }
+  }
 
-    checkForDraw() {
-        if (this.turns === 9 && this.winnerFound === false) {
-            this.draw = true
-            this.isGameLive = false
-            console.log('DRAW') 
-        }
+  checkForDraw() {
+    if (this.turns === 9 && this.winnerFound === false) {
+      this.draw = true;
+      this.isGameLive = false;
+      console.log("DRAW");
     }
+  }
 
-    updateBoard(id) {
-        this.gameBoardSpaces[id] = this.currentTurn
+  updateBoard(id) {
+    this.gameBoardSpaces[id] = this.currentTurn;
+  }
+
+  resetBoard() {
+    this.gameBoardSpaces = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+    this.isGameLive = true;
+    this.winnerFound = false;
+    this.draw = false;
+    this.player1.boardPosition = [];
+    this.player2.boardPosition = [];
+    this.turns = 0;
+    if (this.firstPlayer === "X") {
+      this.firstPlayer = "O";
+      this.currentTurn = "O";
+    } else {
+      this.firstPlayer = "X";
+      this.currentTurn = "X";
     }
-
-    resetBoard() {
-        this.gameBoardSpaces = [0, 0, 0, 0, 0, 0, 0, 0, 0]
-        this.isGameLive = false
-       }
-
-    }
+  }
+}
